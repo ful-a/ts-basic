ece32207c2510e6d63f7a30673130e1bc867170a
ful-a <croqkes@docomo.ne.jp>
2022-01-01 17:25:10 +0900

    add show type with alias

--- src/index.ts
+++ src/index.ts
@@ -14,22 +14,33 @@
 // unknownSample()
 
 // #4 関数での型定義
-import {alwaysThrowError, 
-        logMessage, 
-        logMessage2, 
-        logMessage3, 
-        logMessage4
-      } from './function/basic'
-logMessage("Hello World!!")
-logMessage2("Hello World!!")
-logMessage3("Hello World!!")
-logMessage4("Hello World!!")
-// alwaysThrowError("Error")
+// import {alwaysThrowError, 
+//         logMessage, 
+//         logMessage2, 
+//         logMessage3, 
+//         logMessage4
+//       } from './function/basic'
+// logMessage("Hello World!!")
+// logMessage2("Hello World!!")
+// logMessage3("Hello World!!")
+// logMessage4("Hello World!!")
+// // alwaysThrowError("Error")
 
-import {isUserSignIn, isUserSignIn2, isUserSignIn3, restParameters} from './function/parameters'
-isUserSignIn('abc', 'ful')
-isUserSignIn('def')
-isUserSignIn2('abc')
-console.log( restParameters(100, 200, 300, 400, 500) )
+// import {isUserSignIn, isUserSignIn2, isUserSignIn3, restParameters} from './function/parameters'
+// isUserSignIn('abc', 'ful')
+// isUserSignIn('def')
+// isUserSignIn2('abc')
+// console.log( restParameters(100, 200, 300, 400, 500) )
 
-isUserSignIn3('abc')
+// isUserSignIn3('abc')
+
+// オブジェクト型の利用
+import {objectSample, objectSample2} from './object/object'
+objectSample()
+objectSample2()
+
+import { aliasSample } from './object/alias'
+aliasSample()
+
+
+
---
+++ src/object/alias.ts
@@ -0,0 +1,73 @@
+export const aliasSample = () => {
+  // 再利用可能な型オブジェクトの定義
+  type profile = {
+    gender: string,
+    locatuon: string,
+    age: number
+  }
+  let user: profile = {
+    gender: 'male',
+    locatuon: 'osaka',
+    age: 28
+  }
+  console.log(user);
+
+  let user2: profile = {
+    gender: 'female',
+    locatuon: 'kobe',
+    age: 21
+  }
+  console.log(user2);
+  
+
+  // 合併型(union)と交差型(intersection)
+  type Knight = {
+    hp: number
+    sp: number
+    weapon: string
+    swordSkill: string
+  }
+
+  type Wizard = {
+    hp: number
+    mp: number
+    weapon: string
+    magicSkill: string
+  }
+
+  // 合併型 KnightとWizardどちらかの型を持つ
+  type Adventurer = Knight | Wizard 
+  // 交差型 KnightとWizardが持つ型を全て持っている
+  type Paladin = Knight & Wizard 
+
+  // Knightの型を持つadventurer2
+  const adventurer1: Adventurer = {
+    hp: 100,
+    sp: 30,
+    weapon: '木の剣',
+    swordSkill: '三連斬り',
+  }
+  console.log('騎士:', adventurer1)
+
+  // Wizardの型を持つadventurer2
+  const adventurer2: Adventurer = {
+    hp: 100,
+    mp: 30,
+    weapon: '木の杖',
+    magicSkill: 'ファイヤボール',
+  }
+  console.log('魔法使い:', adventurer2)
+
+  // KnightとWizard両方の型を持つpaladin
+  const paladin: Paladin = {
+    hp: 300,
+    sp: 100,
+    mp: 100,
+    weapon: '銀の剣',
+    swordSkill: '三連斬り',
+    magicSkill: 'ファイヤボール',
+  }
+
+  console.log('パラディン（騎士と魔法使いの両方の能力を持つ）:', paladin)
+
+}
