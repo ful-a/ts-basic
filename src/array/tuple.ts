dd5e901a989cfb250897d9176de9b28676af34d8
ful-a <croqkes@docomo.ne.jp>
2022-01-03 13:33:12 +0900

    fix README

---
+++ src/array/tuple.ts
@@ -0,0 +1,28 @@
+export const tupleSample = () => {
+  // 配列の型定義
+  let responce: [Number, String] = [100, 'foo']
+  responce = [200, 'hoge']
+
+  // 2つの要素が含まれる配列と指定しているので
+  // 指定した値の数は合わないのでエラーが発生する
+  // responce = [300, 'fuge', 'more']
+
+  // 指定した方と違う場合もエラーが出ます
+  // responce = ['fuge', 300]
+  console.log('配列の値それぞれに型を指定', responce);
+}
+
+export const tupleSample2 = () => {
+  // 読み込み専用の配列
+  let readOnlyArray: readonly string[] = ['one', 'two', 'three']
+  // ↓の書き方でも同じ意味です
+  //  readonly -> Readonlyに変わっているので注意
+  // let readOnlyArray: ReadonlyArray<string> = ['one', 'two', 'three']
+  // let readOnlyArray: Readonly<string[]> = ['one', 'two', 'three']
+
+  // 読み込み専用なので追加や書き換えができない
+  // readOnlyArray.push('four')
+  // readOnlyArray[2] = 'four'
+  console.log('読み込み専用の配列', readOnlyArray);
+
+}
